9d9c209d2b117febc45ef6fc8e822c2d864d0ce9
Nikhil Thota <nt92@ufl.edu>
2016-05-28 18:04:16 -0400

    added basic routing

--- Node/library/app.js
+++ Node/library/app.js
@@ -1,1 +1,20 @@
 var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+  res.send('hello');
+});
+
+app.get('/Books', function(req, res){
+  res.send('books!');
+});
+
+app.listen(port, function(err){
+  console.log('running on port ' + port);
+});
