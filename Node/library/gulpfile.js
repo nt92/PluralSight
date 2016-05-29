60ff77fa737d97cf358cff71a7212f3b683ab0fe
Nikhil Thota <nt92@ufl.edu>
2016-05-29 13:45:02 -0400

    added nodemon with gulp

--- Node/library/gulpfile.js
+++ Node/library/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('nodemon');
 
 var jsfiles = ['*.js', 'src/**/*.js'];
 
@@ -11,4 +12,27 @@ gulp.task('style', function() {
             verbose: true
         }))
         .pipe(jscs());
+});
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep);
+});
+
+gulp.task('serve', ['style'] ,function() {
+    var options = {
+        script : 'app.js',
+        delayTime : 1,
+        env : {
+            'PORT' : 3000
+        },
+        watch : jsfiles
+    };
+
+    return nodemon(options)
+        .on('restart', function(ev) {
+            console.log('Restarting...');
+        });
 });
