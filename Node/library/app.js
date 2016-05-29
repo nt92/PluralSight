b22974fbce862fd0d9baaca41cf2b569c7212b9d
Nikhil Thota <nt92@ufl.edu>
2016-05-29 13:32:43 -0400

    added gulp stuff

--- Node/library/app.js
+++ Node/library/app.js
@@ -7,14 +7,14 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
-  res.send('hello');
+app.get('/', function(req, res) {
+    res.send('hello');
 });
 
-app.get('/Books', function(req, res){
-  res.send('books!');
+app.get('/Books', function(req, res) {
+    res.send('books!');
 });
 
-app.listen(port, function(err){
-  console.log('running on port ' + port);
+app.listen(port, function(err) {
+    console.log('running on port ' + port);
 });
